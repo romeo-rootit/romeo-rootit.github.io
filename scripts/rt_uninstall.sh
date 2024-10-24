59db8c6c41e48b964ac10f3680c071ed0619f83d
beeper <beeper@punkto.org>
2024-10-24 16:00:05 -0700

    bashism

--- scripts/rt_uninstall.sh
+++ scripts/rt_uninstall.sh
@@ -3,7 +3,7 @@
 
 echo "Remove ROOT IT support account? (y/N)"
 read choice1
-if [ "${choice1}" == "y" ]; then
+if [ "${choice1}" = "y" ]; then
   userdel rootit
   rm -rf /home/rootit/
   rm /etc/ssh/rootit_tunnel_key
@@ -11,7 +11,7 @@ fi
 
 echo "Remove SSH server? (y/N)"
 read choice2
-if [ "${choice2}" == "y" ]; then
+if [ "${choice2}" = "y" ]; then
   apt purge -y openssh-server
   systemctl stop ssh
 fi
