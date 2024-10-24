0c31bd69d0cbf5f37ca0e16bdd8af2a1dcbd5ff7
beeper <beeper@punkto.org>
2024-10-24 16:12:06 -0700

    read doesn't work in a pipe so we're just gonna send it

--- scripts/rt_uninstall.sh
+++ scripts/rt_uninstall.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-#
 
-echo "Remove ROOT IT support account? (y/N)"
-read choice1
-if [ "${choice1}" = "y" ]; then
-  userdel rootit
-  rm -rf /home/rootit/
-  rm /etc/ssh/rootit_tunnel_key
-fi
-
-echo "Remove SSH server? (y/N)"
-read choice2
-if [ "${choice2}" = "y" ]; then
-  apt purge -y openssh-server
-  systemctl stop ssh
-fi
+echo "Removing ROOT IT Support Account"
+userdel rootit > /dev/null 2>&1
+rm -rf /home/rootit/ > /dev/null 2>&1
+rm /etc/ssh/rootit_tunnel_key > /dev/null 2>&1
